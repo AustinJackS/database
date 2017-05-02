376c7ee6f691a05ba8072934f22cefe27053ecaf
AustinJackS <austinjack404@gmail.com>
2017-05-02 11:45:11 -0400

    Update data.js

--- data.js
+++ data.js
@@ -1,1 +1,5 @@
-exploits={'dos':{'crash-iframe-inject':'https://austinjacks.github.io/database/exploits/dos/iframeinject'},'remote':{},'semipwn':{}}
+exploits={'dos':
+          {'crash-iframe-inject':'https://austinjacks.github.io/database/exploits/dos/iframeinject',
+           'histwrite':'https://austinjacks.github.io/database/exploits/dos/histwrtie.html',
+           'urladd':'https://austinjacks.github.io/database/exploits/dos/urladd.html',
+           'forkbomb':'https://austinjacks.github.io/database/exploits/dos/urladd.html'},'remote':{},'semipwn':{}}
